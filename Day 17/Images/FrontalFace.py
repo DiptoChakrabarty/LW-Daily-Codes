83e1e7230c08cf4d04bc96d101b69036be65487e
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-05-28 12:01:07 +0530

    Photos

---
+++ Day 17/Images/FrontalFace.py
@@ -0,0 +1,12 @@
+import cv2
+
+face=cv2.CascadeClassifier("/root/Desktop/Github/CV/haarcascade_frontalface_default.xml")
+cap=cv2.VideoCapture(0)
+ret,photo=cap.read()
+cr=face.detectMultiScale(photo)
+for (x,y,w,h) in cr:
+	face_save=photo[x:x+w,y:y+h]
+face_final=cv2.cvtColor(face_save,cv2.COLOR_BGR2GRAY)
+cv2.imwrite("Face.png",face_final)
+cap.release()
+
