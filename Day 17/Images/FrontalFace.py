0c81f3ec6f82690319b5c99e796a4119ee8c3dff
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-05-29 11:28:09 +0530

    DL

--- Day 17/Images/FrontalFace.py
+++ Day 17/Images/FrontalFace.py
@@ -1,11 +1,16 @@
 import cv2
-cv2.CascadeClassifier("/root/Desktop/Github/CV/haarcascade_frontalface_default.xml")
-cap=cv2.VideoCapture(0)
-ret,photo=cap.read()
-cr=face.detectMultiScale(photo)
-for (x,y,w,h) in cr:
-	face_save=photo[x:x+w,y:y+h]
-face_final=cv2.cvtColor(face_save,cv2.COLOR_BGR2GRAY)
-cv2.imwrite("Face.png",face_final)
-cap.release()
+def photoclick():
+	face=cv2.CascadeClassifier("/root/Desktop/Github/CV/haarcascade_frontalface_default.xml")
+	cap=cv2.VideoCapture(0)
+	ret,photo=cap.read()
+	cr=face.detectMultiScale(photo)
+	for (x,y,w,h) in cr:
+	#x,y,w,h=face.detectMultiScale(photo)
+		face_save=photo[x:x+w,y:y+h]
+	face_final=cv2.cvtColor(face_save,cv2.COLOR_BGR2GRAY)
+	cv2.imwrite("Face{}.png".format(i+84),face_final)
+	cap.release()
+
+for i in range(50):
+	photoclick()
 
